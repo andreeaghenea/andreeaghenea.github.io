9c98aafc15856efb6fb77e73ecfc3a8466b8ab24
RetroCIB <retrocib@gmail.com>
2020-02-15 17:35:00 +0200

    contact (wip)

--- assets/script.js
+++ assets/script.js
@@ -356,6 +356,70 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: "SectionContactMe",
   props: {},
@@ -569,6 +633,25 @@ exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loa
 
 // module
 exports.push([module.i, "@media (min-width: 992px) {\nheader[data-v-24bb1bb9] {\n    position: fixed;\n    top: 0;\n    right: 0;\n    left: 0;\n    z-index: 1030;\n}\n}", ""]);
+
+// exports
+
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./src/js/components/section_contact_me.vue?vue&type=style&index=0&id=f5992e46&scoped=true&lang=scss&":
+/*!**********************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--7-2!./node_modules/sass-loader/dist/cjs.js??ref--7-3!./node_modules/vue-loader/lib??vue-loader-options!./src/js/components/section_contact_me.vue?vue&type=style&index=0&id=f5992e46&scoped=true&lang=scss& ***!
+  \**********************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "p[data-v-f5992e46] {\n  overflow: hidden;\n  clear: both;\n  color: #666;\n}\np span[data-v-f5992e46] {\n  display: inline-block;\n  color: #18c9de;\n  margin-right: 25px;\n  font-size: 20px;\n  width: 60px;\n  height: 60px;\n  line-height: 60px;\n  border-radius: 50%;\n  background-color: #f7faff;\n  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);\n  text-align: center;\n}\np a[data-v-f5992e46] {\n  -webkit-transition: all 0.3s ease-in-out;\n  transition: all 0.3s ease-in-out;\n}\np a[data-v-f5992e46]:hover {\n  color: #18c9de;\n}\n.form-control[data-v-f5992e46] {\n  display: block;\n  width: 100%;\n  height: 50px;\n  line-height: 50px;\n  padding: 6px 15px;\n  border-top: 1px solid transparent;\n  border-left: 1px solid transparent;\n  border-right: 1px solid transparent;\n  border-bottom: 1px solid #ddd;\n  margin-bottom: 15px;\n  border-radius: 4px;\n  background: #f7faff;\n  position: relative;\n  box-shadow: inherit;\n}\ntextarea.form-control[data-v-f5992e46] {\n  margin: 0px -6.5px 15px 0px;\n  height: 259px;\n  line-height: 35px;\n}", ""]);
 
 // exports
 
@@ -15372,6 +15455,36 @@ if(false) {}
 
 /***/ }),
 
+/***/ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./src/js/components/section_contact_me.vue?vue&type=style&index=0&id=f5992e46&scoped=true&lang=scss&":
+/*!**************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/style-loader!./node_modules/css-loader!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--7-2!./node_modules/sass-loader/dist/cjs.js??ref--7-3!./node_modules/vue-loader/lib??vue-loader-options!./src/js/components/section_contact_me.vue?vue&type=style&index=0&id=f5992e46&scoped=true&lang=scss& ***!
+  \**************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+var content = __webpack_require__(/*! !../../../node_modules/css-loader!../../../node_modules/vue-loader/lib/loaders/stylePostLoader.js!../../../node_modules/postcss-loader/src??ref--7-2!../../../node_modules/sass-loader/dist/cjs.js??ref--7-3!../../../node_modules/vue-loader/lib??vue-loader-options!./section_contact_me.vue?vue&type=style&index=0&id=f5992e46&scoped=true&lang=scss& */ "./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./src/js/components/section_contact_me.vue?vue&type=style&index=0&id=f5992e46&scoped=true&lang=scss&");
+
+if(typeof content === 'string') content = [[module.i, content, '']];
+
+var transform;
+var insertInto;
+
+
+
+var options = {"hmr":true}
+
+options.transform = transform
+options.insertInto = undefined;
+
+var update = __webpack_require__(/*! ../../../node_modules/style-loader/lib/addStyles.js */ "./node_modules/style-loader/lib/addStyles.js")(content, options);
+
+if(content.locals) module.exports = content.locals;
+
+if(false) {}
+
+/***/ }),
+
 /***/ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./src/js/components/section_home.vue?vue&type=style&index=0&id=125f8bb6&scoped=true&lang=scss&":
 /*!********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************!*\
   !*** ./node_modules/style-loader!./node_modules/css-loader!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src??ref--7-2!./node_modules/sass-loader/dist/cjs.js??ref--7-3!./node_modules/vue-loader/lib??vue-loader-options!./src/js/components/section_home.vue?vue&type=style&index=0&id=125f8bb6&scoped=true&lang=scss& ***!
@@ -18288,10 +18401,180 @@ var render = function() {
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
   return _c("section", { attrs: { id: "SectionContactMe" } }, [
-    _vm._v("\n    SectionContactMe\n")
+    _c("div", { staticClass: "row" }, [
+      _vm._m(0),
+      _vm._v(" "),
+      _c("div", { staticClass: "col-12 col-lg-8" }, [
+        _c(
+          "form",
+          {
+            on: {
+              submit: function($event) {
+                $event.stopPropagation()
+                $event.preventDefault()
+                return _vm.submit()
+              }
+            }
+          },
+          [_vm._m(1)]
+        )
+      ]),
+      _vm._v(" "),
+      _vm._m(2)
+    ])
   ])
 }
-var staticRenderFns = []
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "col-12 mb-5" }, [
+      _c("h2", { staticClass: "text-center" }, [_vm._v("Contact Me")]),
+      _vm._v(" "),
+      _c("p", { staticClass: "text-center pl-5 pr-5" }, [
+        _vm._v(
+          "\n                It is a long established fact that a reader will be distracted by the readable\n                content of a page when looking at its layout\n            "
+        )
+      ])
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "row" }, [
+      _c("div", { staticClass: "form-group col-md-4" }, [
+        _c("input", {
+          staticClass: "form-control",
+          attrs: {
+            type: "text",
+            name: "name",
+            id: "first-name",
+            placeholder: "Full Name",
+            required: "required"
+          }
+        })
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "form-group col-md-4" }, [
+        _c("input", {
+          staticClass: "form-control",
+          attrs: {
+            type: "email",
+            name: "email",
+            id: "emailn",
+            placeholder: "Email",
+            required: "required"
+          }
+        })
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "form-group col-md-4" }, [
+        _c("input", {
+          staticClass: "form-control",
+          attrs: {
+            type: "text",
+            name: "subject",
+            id: "subject",
+            placeholder: "Subject",
+            required: "required"
+          }
+        })
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "form-group col-md-12" }, [
+        _c("textarea", {
+          staticClass: "form-control",
+          attrs: {
+            rows: "6",
+            name: "message",
+            id: "description",
+            placeholder: "Your Message",
+            required: "required"
+          }
+        })
+      ]),
+      _vm._v(" "),
+      _c("div", { staticClass: "col-md-12 text-center" }, [
+        _c("div", { staticClass: "actions" }, [
+          _c("input", {
+            staticClass: "main-btn",
+            attrs: {
+              type: "submit",
+              value: "Send Me",
+              name: "submit",
+              id: "submitButton"
+            }
+          })
+        ])
+      ])
+    ])
+  },
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "col-12 col-lg-4" }, [
+      _c("h4", [_vm._v("Andreea Ghenea")]),
+      _vm._v(" "),
+      _c("p", [_vm._v("Web Design & Web Development")]),
+      _vm._v(" "),
+      _c("p", { staticClass: "mb-3 pb-2" }, [
+        _c("span", {
+          staticClass: "icon-map-marked-alt d-block d-sm-inline-block mb-2"
+        }),
+        _vm._v(" "),
+        _c(
+          "a",
+          {
+            attrs: {
+              href:
+                "https://www.google.de/maps/place/Munich,+Germany/@48.1360411,11.5977896,12z",
+              target: "_blank"
+            }
+          },
+          [
+            _vm._v(
+              "\n                    München, Deutschland\n                "
+            )
+          ]
+        )
+      ]),
+      _vm._v(" "),
+      _c("p", { staticClass: "mb-3 pb-2" }, [
+        _c("span", {
+          staticClass: "icon-headset d-block d-sm-inline-block mb-2"
+        }),
+        _vm._v(" "),
+        _c("a", { attrs: { href: "tel:00491737078695", target: "_blank" } }, [
+          _vm._v("\n                    (0049) 173 707 8695\n                ")
+        ])
+      ]),
+      _vm._v(" "),
+      _c("p", { staticClass: "mb-3 pb-2" }, [
+        _c("span", {
+          staticClass: "icon-envelope d-block d-sm-inline-block mb-2"
+        }),
+        _vm._v(" "),
+        _c(
+          "a",
+          {
+            attrs: {
+              href: "mailto:contact@angreeaghenea.com",
+              target: "_blank"
+            }
+          },
+          [
+            _vm._v(
+              "\n                    contact@angreeaghenea.com\n                "
+            )
+          ]
+        )
+      ])
+    ])
+  }
+]
 render._withStripped = true
 
 
@@ -31210,7 +31493,9 @@ __webpack_require__.r(__webpack_exports__);
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _section_contact_me_vue_vue_type_template_id_f5992e46_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./section_contact_me.vue?vue&type=template&id=f5992e46&scoped=true& */ "./src/js/components/section_contact_me.vue?vue&type=template&id=f5992e46&scoped=true&");
 /* harmony import */ var _section_contact_me_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./section_contact_me.vue?vue&type=script&lang=js& */ "./src/js/components/section_contact_me.vue?vue&type=script&lang=js&");
-/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* empty/unused harmony star reexport *//* harmony import */ var _section_contact_me_vue_vue_type_style_index_0_id_f5992e46_scoped_true_lang_scss___WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./section_contact_me.vue?vue&type=style&index=0&id=f5992e46&scoped=true&lang=scss& */ "./src/js/components/section_contact_me.vue?vue&type=style&index=0&id=f5992e46&scoped=true&lang=scss&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
 
 
 
@@ -31218,7 +31503,7 @@ __webpack_require__.r(__webpack_exports__);
 
 /* normalize component */
 
-var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_3__["default"])(
   _section_contact_me_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
   _section_contact_me_vue_vue_type_template_id_f5992e46_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"],
   _section_contact_me_vue_vue_type_template_id_f5992e46_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
@@ -31247,6 +31532,22 @@ component.options.__file = "src/js/components/section_contact_me.vue"
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_section_contact_me_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./section_contact_me.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./src/js/components/section_contact_me.vue?vue&type=script&lang=js&");
 /* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_section_contact_me_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./src/js/components/section_contact_me.vue?vue&type=style&index=0&id=f5992e46&scoped=true&lang=scss&":
+/*!************************************************************************************************************!*\
+  !*** ./src/js/components/section_contact_me.vue?vue&type=style&index=0&id=f5992e46&scoped=true&lang=scss& ***!
+  \************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_sass_loader_dist_cjs_js_ref_7_3_node_modules_vue_loader_lib_index_js_vue_loader_options_section_contact_me_vue_vue_type_style_index_0_id_f5992e46_scoped_true_lang_scss___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/style-loader!../../../node_modules/css-loader!../../../node_modules/vue-loader/lib/loaders/stylePostLoader.js!../../../node_modules/postcss-loader/src??ref--7-2!../../../node_modules/sass-loader/dist/cjs.js??ref--7-3!../../../node_modules/vue-loader/lib??vue-loader-options!./section_contact_me.vue?vue&type=style&index=0&id=f5992e46&scoped=true&lang=scss& */ "./node_modules/style-loader/index.js!./node_modules/css-loader/index.js!./node_modules/vue-loader/lib/loaders/stylePostLoader.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./node_modules/vue-loader/lib/index.js?!./src/js/components/section_contact_me.vue?vue&type=style&index=0&id=f5992e46&scoped=true&lang=scss&");
+/* harmony import */ var _node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_sass_loader_dist_cjs_js_ref_7_3_node_modules_vue_loader_lib_index_js_vue_loader_options_section_contact_me_vue_vue_type_style_index_0_id_f5992e46_scoped_true_lang_scss___WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_sass_loader_dist_cjs_js_ref_7_3_node_modules_vue_loader_lib_index_js_vue_loader_options_section_contact_me_vue_vue_type_style_index_0_id_f5992e46_scoped_true_lang_scss___WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_sass_loader_dist_cjs_js_ref_7_3_node_modules_vue_loader_lib_index_js_vue_loader_options_section_contact_me_vue_vue_type_style_index_0_id_f5992e46_scoped_true_lang_scss___WEBPACK_IMPORTED_MODULE_0__) if(__WEBPACK_IMPORT_KEY__ !== 'default') (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_sass_loader_dist_cjs_js_ref_7_3_node_modules_vue_loader_lib_index_js_vue_loader_options_section_contact_me_vue_vue_type_style_index_0_id_f5992e46_scoped_true_lang_scss___WEBPACK_IMPORTED_MODULE_0__[key]; }) }(__WEBPACK_IMPORT_KEY__));
+ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_style_loader_index_js_node_modules_css_loader_index_js_node_modules_vue_loader_lib_loaders_stylePostLoader_js_node_modules_postcss_loader_src_index_js_ref_7_2_node_modules_sass_loader_dist_cjs_js_ref_7_3_node_modules_vue_loader_lib_index_js_vue_loader_options_section_contact_me_vue_vue_type_style_index_0_id_f5992e46_scoped_true_lang_scss___WEBPACK_IMPORTED_MODULE_0___default.a); 
 
 /***/ }),
 
--- src/js/components/section_contact_me.vue
+++ src/js/components/section_contact_me.vue
@@ -1,6 +1,70 @@
 <template>
     <section id="SectionContactMe">
-        SectionContactMe
+        <div class="row">
+            <div class="col-12 mb-5">
+                <h2 class="text-center">Contact Me</h2>
+                <p class="text-center pl-5 pr-5">
+                    It is a long established fact that a reader will be distracted by the readable
+                    content of a page when looking at its layout
+                </p>
+            </div>
+
+            <div class="col-12 col-lg-8">
+                <form @submit.stop.prevent="submit()">
+                    <div class="row">
+                        <div class="form-group col-md-4">
+                            <input type="text" name="name" class="form-control" id="first-name" placeholder="Full Name" required="required">
+                        </div>
+
+                        <div class="form-group col-md-4">
+                            <input type="email" name="email" class="form-control" id="emailn" placeholder="Email" required="required">
+                        </div>
+
+                        <div class="form-group col-md-4">
+                            <input type="text" name="subject" class="form-control" id="subject" placeholder="Subject" required="required">
+                        </div>
+
+                        <div class="form-group col-md-12">
+                            <textarea rows="6" name="message" class="form-control" id="description" placeholder="Your Message" required="required"></textarea>
+                        </div>
+
+                        <div class="col-md-12 text-center">
+                            <div class="actions">
+                                <input type="submit" value="Send Me" name="submit" id="submitButton" class="main-btn">
+                            </div>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+
+            <div class="col-12 col-lg-4">
+                <h4 >Andreea Ghenea</h4>
+
+                <p>Web Design & Web Development</p>
+                <p class="mb-3 pb-2">
+                    <span class="icon-map-marked-alt d-block d-sm-inline-block mb-2"></span>
+                    <a href="https://www.google.de/maps/place/Munich,+Germany/@48.1360411,11.5977896,12z"
+                       target="_blank">
+                        München, Deutschland
+                    </a>
+                </p>
+                <p class="mb-3 pb-2">
+                    <span class="icon-headset d-block d-sm-inline-block mb-2"></span>
+                    <a href="tel:00491737078695" target="_blank">
+                        (0049) 173 707 8695
+                    </a>
+                </p>
+                <p class="mb-3 pb-2">
+                    <span class="icon-envelope d-block d-sm-inline-block mb-2"></span>
+                    <a href="mailto:contact@angreeaghenea.com" target="_blank">
+                        contact@angreeaghenea.com
+                    </a>
+                </p>
+            </div>
+        </div>
+
+
     </section>
 </template>
 <script>
@@ -13,5 +77,59 @@
     };
 </script>
 <style scoped lang="scss">
+    @import "src/sass/variables";
+
+    p {
+        overflow: hidden;
+        clear: both;
+        color: #666;
+
+        span {
+            display: inline-block;
+            color: #18c9de;
+            margin-right: 25px;
+            font-size: 20px;
+            width: 60px;
+            height: 60px;
+            line-height: 60px;
+            border-radius: 50%;
+            background-color: #f7faff;
+            box-shadow: 0 5px 15px rgba(0, 0, 0, .1);
+            text-align: center;
+        }
+
+        a {
+            transition: all 0.3s ease-in-out;
+
+            &:hover {
+                color: $color_primary;
+            }
+        }
+    }
+
+    .form-control {
+        display: block;
+        width: 100%;
+        height: 50px;
+        line-height: 50px;
+        padding: 6px 15px;
+        border-top: 1px solid transparent;
+        border-left: 1px solid transparent;
+        border-right: 1px solid transparent;
+        border-bottom: 1px solid #ddd;
+        margin-bottom: 15px;
+        border-radius: 4px;
+        background: #f7faff;
+        position: relative;
+        box-shadow: inherit;
+
+    }
+
+    textarea.form-control  {
+        margin: 0px -6.5px 15px 0px;
+        height: 259px;
+        line-height: 35px;
+    }
+
 
 </style>
